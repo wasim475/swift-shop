ffa7f48ff5824d707e27f118a1d7f27be730bf06
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-19 08:47:10 +0600

    Update categoryCard.js

--- swift-shop/component/category/categoryCard.js
+++ swift-shop/component/category/categoryCard.js
@@ -1,21 +1,24 @@
 'use client'
 import Image from 'next/image';
 
-const CategoryCard = () => {
+const CategoryCard = ({item}) => {
+    console.log(item)
+    const {name, price, imageLink, description}= item
   return (
     <>
       <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
         <img
-          src="https://source.unsplash.com/random/300x300/?1"
+            
+          src={imageLink}
           alt=""
           className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
         />
         <div className="mt-6 mb-2">
           <span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-600">
-            Quisque
+            ${name}
           </span>
           <h2 className="text-xl font-semibold tracking-wide">
-            Nam maximus purus
+          ${price}
           </h2>
         </div>
         <p className="dark:text-gray-800">
